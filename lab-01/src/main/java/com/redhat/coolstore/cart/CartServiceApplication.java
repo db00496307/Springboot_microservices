aabb19bba0321804a591bf80ffbf3ff8036ff1cb
Deepika Bandameedi <DB00496307@techmahindra.com>
2018-11-09 16:19:56 +0530

    commit3

--- lab-01/src/main/java/com/redhat/coolstore/cart/CartServiceApplication.java
+++ lab-01/src/main/java/com/redhat/coolstore/cart/CartServiceApplication.java
@@ -1,10 +1,11 @@
 package com.redhat.coolstore.cart;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+
 @SpringBootApplication
 public class CartServiceApplication {
-
-    public static void main(String[] args) {
+		public static void main(String[] args) {
     	SpringApplication.run(CartServiceApplication.class, args);
     	
     }
